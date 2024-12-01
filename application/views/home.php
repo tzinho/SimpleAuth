001a1424eb713331283db52c26834c025b53c4ca
tzinho <tzinho.indie@gmail.com>
2024-12-01 20:55:36 -0300

    chore: add tailwind config

--- application/views/home.php
+++ application/views/home.php
@@ -5,11 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<link href="<?= base_url('public/css/output.css') ?>" rel="stylesheet">
-
 	<title>Home</title>
-	
 </head>
 <body>
-<h1 class="text-xl text-green-300">Home</h1>
+	<div class="flex min-h-screen items-center justify-center font-semibold">
+  <h1 class="text-2xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text"><span class="text-3xl">Simple</span> template <span class="text-3xl">CodeIgniter</span> with <span class="text-3xl">TailwindCSS</span></h1>
+	</div>
 </body>
 </html>
